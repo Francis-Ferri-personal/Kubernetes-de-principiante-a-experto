aeece3659ee8e62b7e25f0d6cc7f50b40e654f42
Francis Ferri <francisferrir@gmail.com>
2021-05-06 20:34:17 -0500

    App express

---
+++ app-access-cluster/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const port = 3000;
+
+const targetURL = '192.168.49.2';
+const targetPort = 31000;
+
+app.get('/', function (req, res) {
+    axios.get(`http://${url}:${port}`)
+        .then( (response) => {
+            res.send(response.data)
+        }
+        )
+        .catch(console.error)
+})
+ 
+app.listen(port, () => 
+    console.log(`Example app listening at http://localhost:${port}`))
